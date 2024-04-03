1ff1dc1dda4bd949880138583bec507cb1c594b2
MohammadAlfarwan <alfrwan58@gmail.com>
2024-04-03 08:28:14 +0300

    Editing for image url

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+'use strict'
+
+let contSec = document.getElementById("containersec");
+let EmployeeForm = document.getElementById("employeeForm");
+let departmentsArr =  ["Administration", "Finance", "Marketing", "Development" ]
+
+let p1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "" )
+let p2 = new Employee(1001, "Lana Ali", "Finance", "Senior", "" )
+let p3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "" )
+let p4 = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "" )
+let p5 = new Employee(1004, "Omar Zaid", "Development", "Senior", "" )
+let p6 = new Employee(1005, "Rana Saleh", "Development", "Junior", "" )
+let p7 = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "" )
+
+let employees = [p1, p2, p3, p4, p5, p6, p7];
 
 function Employee(employeeID, fullName, department, level, imageURL, salary) {
     this.employeeID = employeeID;
@@ -7,18 +22,6 @@ function Employee(employeeID, fullName, department, level, imageURL, salary) {
     this.imageURL = imageURL;
     this.salary = salary;
 }
-
-let p1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p2 = new Employee(1001, "Lana Ali", "Finance", "Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p4 = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p5 = new Employee(1004, "Omar Zaid", "Development", "Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p6 = new Employee(1005, "Rana Saleh", "Development", "Junior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-let p7 = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU" )
-
-let departmentsArr =  ["Administration", "Finance", "Marketing", "Development" ]
-let employees = [p1, p2, p3, p4, p5, p6, p7];
-
 
 
 Employee.prototype.calcSalary = function () {
@@ -37,11 +40,6 @@ Employee.prototype.calcNetSalary = function() {
     return Math.floor(this.salary) ;
 }
 
-
-
-
-let contSec = document.getElementById("containersec");
-
 Employee.prototype.render = function () {
     // Create elements
     let infoEmp = document.createElement("div");
@@ -53,22 +51,25 @@ Employee.prototype.render = function () {
     // Set attributes
     img.setAttribute("height", "100");
     img.setAttribute("width", "100");
-    if (
-        this.employeeID === 1000 || 
-        this.employeeID === 1001 || 
-        this.employeeID === 1002 || 
-        this.employeeID === 1003 || 
-        this.employeeID === 1004 || 
-        this.employeeID === 1005 || 
-        this.employeeID === 1006
-        ) {
-
-            img.setAttribute("src", `./../assets/${this.fullName.split(' ')[0]}.jpg`);
+    if (this.imageURL === "") {
+        if (
+            this.employeeID === 1000 || 
+            this.employeeID === 1001 || 
+            this.employeeID === 1002 || 
+            this.employeeID === 1003 || 
+            this.employeeID === 1004 || 
+            this.employeeID === 1005 || 
+            this.employeeID === 1006
+            ) {
+                img.setAttribute("src", `./../assets/${this.fullName.split(' ')[0]}.jpg`);
+        } else {
+            img.setAttribute("src", `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU`);
+        }
     } else {
-        img.setAttribute("src", `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU`);
+        img.setAttribute("src", `${this.imageURL}`);
     }
     img.setAttribute("alt", "Employee Image");
-
+    
     // Set text content
     let fullNameText = document.createTextNode(`Full Name: ${this.fullName}`);
     let employeeIDText = document.createTextNode(`Employee ID: ${this.employeeID}`);
@@ -76,7 +77,7 @@ Employee.prototype.render = function () {
     let levelText = document.createTextNode(`Level: ${this.level}`);
     let salaryText = document.createTextNode(`Salary: ${this.calcSalary()}`);
     let netSalaryText = document.createTextNode(`Net Salary: ${this.calcNetSalary()}`);
-
+    
     // Append text nodes to infoDiv
     infoDiv.appendChild(fullNameText);
     infoDiv.appendChild(document.createElement("br"));
@@ -91,103 +92,57 @@ Employee.prototype.render = function () {
     infoDiv.appendChild(netSalaryText);
     infoDiv.appendChild(document.createElement("br"));
     infoDiv.appendChild(document.createElement("br"));
-
+    
     // Append elements to their parents
     cardDiv.appendChild(img);
     cardDiv.appendChild(infoDiv);
     infoEmp.appendChild(cardDiv);
     contSec.appendChild(infoEmp);
     document.body.appendChild(contSec);
-
-
+    
+    
 }
 
-
-
-
-
-    function renderSection(employees, departmentsArr) {
-        departmentsArr.forEach(dep => {
-            contSec.innerHTML += `<div class="main-heading"><h2 class= "seperate">${dep}</h2></div>`;
-            // console.log(dep);
-            employees.forEach(p => {
-                if (dep === p.department) {
-                    p.render();
-                    // console.log(p);
-                }
-            });
-            // console.log("=========================================================================");
+function renderSection(employees, departmentsArr) {
+    departmentsArr.forEach(dep => {
+        contSec.innerHTML += `<div class="main-heading"><h2 class= "seperate">${dep}</h2></div>`;
+        // console.log(dep);
+        employees.forEach(p => {
+            if (dep === p.department) {
+                p.render();
+                // console.log(p);
+            }
         });
+        // console.log("=========================================================================");
+    });
+}   
+// To generate ID number  
+let employeeCounter = 1007; // Start the counter at 1000
+function generateId() {
+    if (employeeCounter >= 10000) {
+        return -1;
     }
     
-    
-    renderSection(employees, departmentsArr);
-    
-
-
-    // p1.render();
-    // p2.render();
-    // p3.render();
-    // p4.render();
-    // p5.render();
-    // p6.render();
-    // p7.render();
-
-   
-   
-
-  
-    
-    // start lab 08
-
-
-    // To generate ID number  
-    let employeeCounter = 1000; // Start the counter at 1000
-    function generateId() {
-        if (employeeCounter >= 10000) {
-            return -1;
-        }
-        
-        // Increment the counter and convert it to a string
-        let id = employeeCounter++;
-        return id.toString();
-    }
-
-    // To add an event listener
-    const departmentsSelect = document.getElementById('departments');
-
-
+    // Increment the counter and convert it to a string
+    let id = employeeCounter++;
+    return id.toString();
+}
 
 //Events
-let EmployeeForm = document.getElementById("employeeForm");
 EmployeeForm.addEventListener('submit', addNewEmployeeHandler);
-
 function addNewEmployeeHandler(event) {
     event.preventDefault();
-    console.log(event);
+    // console.log(event);
     let empName = event.target.fullName.value;
     let empDepartment = event.target.departments.value;
     let empLevel = event.target.level.value;
     let imgPath = event.target.image.value;
-
-    let newEmployee = new Employee(generateId(),empName, empDepartment,empLevel, imgPath || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU");
+    // Ask your TA about this || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0e-9L-OuQW5Dqfbaqlpl84ptS0VWZbY1K_A&usqp=CAU"
+    let newEmployee = new Employee(generateId(),empName, empDepartment,empLevel, imgPath );
     // console.log(newEmployee);
     employees.push(newEmployee);
     contSec.innerHTML = "";
     renderSection(employees, departmentsArr);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    // end lab 08
-
+renderSection(employees, departmentsArr);
